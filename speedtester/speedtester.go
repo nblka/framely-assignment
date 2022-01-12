c8676503d724954b303eba83dac4b05271f36376
Pavel Karagodin <nblka@alohabrowser.com>
2022-01-12 14:24:30 +0700

    introduce service interface

--- speedtester/speedtester.go
+++ speedtester/speedtester.go
@@ -18,7 +18,20 @@ func (m Measure) String() string {
 	return fmt.Sprintf("Download %v Mbps, upload %v Mbps", m.Download, m.Upload)
 }
 
+type iService interface {
+	Run() Measure
+}
+
+func createService(s Service) (iService, error) {
+	// TODO
+	return nil, fmt.Errorf("unknown service")
+}
+
 // returns download and upload speed
-func Test(s Service) Measure {
-	return Measure{0.0, 0.0}
+func Test(s Service) (Measure, error) {
+	service, err := createService(s)
+	if err != nil {
+		return Measure{0.0, 0.0}, err
+	}
+	return service.Run(), nil
 }
